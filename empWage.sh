3c687159ddfc7bff25d423c52f95e0a334bdea69
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-07-27 17:27:13 +0530

    Full Time

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+FULLTIME=0
+EMPRATEPERHOUR=20
+
 random=$((RANDOM%2))
 
 	if (( $random == 0 ))
 	then
-	echo "Present"
-	else
-	echo "Absent"
+	empHours=8
 	fi
+	salary=$(($EMPRATEPERHOUR*$empHours))
+        echo $salary
