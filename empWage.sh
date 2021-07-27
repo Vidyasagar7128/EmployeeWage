01bd4add1086800660b5324c81cc770801e3e118
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-07-27 17:33:39 +0530

    Using Case

--- empWage.sh
+++ empWage.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 FULLTIME=0
+PARTTIME=1
+FULLTIME=0
 EMPRATEPERHOUR=20
 
-random=$((RANDOM%2))
+random=$((RANDOM%3))
+case $random in
+        $PARTTIME)
+        empHours=4
+        ;;
+        $FULLTIME)
+        empHours=8
+        ;;
+        *)
+        empHours=0
+        ;;
+esac
 
-	if (( $random == 1 ))
-	then
-	empHours=4
-	fi
 	salary=$(($EMPRATEPERHOUR*$empHours))
-        echo $salary
+        echo "Using Case :" $salary
